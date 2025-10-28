8c167eb81fa673cce55cd3229779dfbe4893b52d
Kgadi-Kom11 <kgadimiane@gmail.com>
2025-10-28 14:17:10 +0200

    Added the getHumanChoice function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,6 @@
 
 
-function getComputerChoice() {
+/*function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
 
     if (randomChoice === 1) {
@@ -14,7 +14,24 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+*/
+//let computerChoice = getComputerChoice();
 
-let computerChoice = getComputerChoice();
+//console.log(computerChoice);
 
-console.log(computerChoice);
+function getHumanChoice() {
+    let humanChoice = window.prompt("Your Choice: ", "Rock")
+
+    if (humanChoice === "Rock") {
+        return "Rock";
+    }
+    else if(humanChoice === "Paper") {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+let mychoice = getHumanChoice();
+console.log(mychoice)
