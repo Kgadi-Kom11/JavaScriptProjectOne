eccf1ded76dcdbc03e841e409645b9cfc46849ea
Kgadi-Kom11 <kgadimiane@gmail.com>
2025-10-28 12:12:10 +0200

    created the getComputerChoice function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,16 @@
+let randomChoice = Math.floor(Math.random() * 3) + 1;
+
+function getComputerChoice() {
+    if () {
+        return "rock";
+    }
+    elseif () {
+        return "paper";
+    }
+    else 
+    {
+        return "scissors";
+    }
+}
+
 console.log("Hello, world!");
