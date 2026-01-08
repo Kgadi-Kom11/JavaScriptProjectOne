c295fe66c31d4a45006557bcb1f19f8ba2fc5392
Kgadi-Kom11 <kgadimiane@gmail.com>
2026-01-08 13:14:37 +0200

    Adding code for rock paper scissors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,79 +1,120 @@
-let humanScore = 0;
+const buttons = document.querySelectorAll("button");
+const userChoiceDisplay = document.querySelector("#user-choice");
+const computerChoiceDisplay = document.querySelector("#computer-choice");
+const resultChoiceDisplay = document.querySelector(".results");
+const userScoreDisplay = document.querySelector("#userScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+const roundDisplay = document.querySelector("#round");
+const finalWinnerDisplay = document.querySelector(".final-winner");
+const reset = document.querySelector("#resetGame")
+
+let userChoice;
+let compChoice;
+let result;
+let userScore = 0;
 let computerScore = 0;
+let currentRound = 1;
+let totalRounds = 5
 
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
 
-    if (computerChoice === 1) {
-        return "rock";
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        userChoice = e.target.textContent;
+        userChoiceDisplay.textContent = "User Choice: " + userChoice;
+        computerChoice()
+        playRound(userChoice, compChoice)
+        runningScore(userScore, computerScore)
+    })
+})
+
+reset.addEventListener("click", () => {
+        resetGameRound()
+    })
+
+function computerChoice() {
+    let computerChoice = Math.floor(Math.random() * buttons.length);
+    if (currentRound <= totalRounds) {
+        if (computerChoice === 0) {
+            compChoice = "Rock";
+        }
+        else if (computerChoice === 1) {
+            compChoice = "Paper";
+        }
+        else if (computerChoice === 2) {
+            compChoice = "Scissors"
+        }
     }
-    else if (computerChoice === 2) {
-        return "paper";
+
+    computerChoiceDisplay.textContent ="Computer Choice: " + compChoice;
+    //runningScore(computerScore)
+}
+
+function playRound(userChoice, compChoice) {
+    if (currentRound <= totalRounds) {
+        if (userChoice === compChoice) {
+            result = "Tied with Computer!";
+        }
+        else if (userChoice === "Rock" && compChoice === "Scissors") {
+            result = "User Win! Rock beats Scissors!";
+            userScore++;
+        }
+        else if (userChoice === "Paper" && compChoice === "Rock") {
+            result = "User Win! Paper Covers Rock!";
+            userScore++; 
+        }
+        else if (userChoice === "Scissors" && compChoice === "Paper") {
+            result = "User Win! Scissors cuts Paper!";
+            userScore++; 
+        }
+        else if (userChoice === "Paper" && compChoice === "Scissors") {
+            result = "Computer Wins! User Lose! Scissors cuts Paper!";
+            computerScore++; 
+        }
+        else if (userChoice === "Scissors" && compChoice === "Rock") {
+            result = "Computer Wins! User Lose! Rock beats Scissors!";
+            computerScore++; 
+        }
+        else if (userChoice === "Rock" && compChoice === "Paper") {
+            result = "Computer Wins! User Lose! Paper Covers Rock!";
+            computerScore++;
+        }
+
+        roundDisplay.textContent = "Round: " + currentRound + " of " + totalRounds;
+        currentRound++
+    }  
+    
+    if (currentRound < totalRounds) {
+        runningScore()
     }
-    else 
-    {
-        return "scissors";
+
+    resultChoiceDisplay.textContent = result;
+    userScoreDisplay.textContent = "User Score: " + userScore;
+    computerScoreDisplay.textContent = "Computer Score: " + computerScore;
+}
+
+function runningScore(userScore, computerScore) {
+    
+    if (userScore > computerScore) {
+        finalWinnerDisplay.textContent = "Congratulations User! You have won the game!!"
+    }
+    else if (userScore < computerScore)  {
+        finalWinnerDisplay.textContent = "Congratulations Computer! You have won the game!!";
+    }
+    else  {
+        finalWinnerDisplay.textContent = "Sorry, You tied with the computer."
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = window.prompt("Your Choice: ")
+function resetGameRound() {
+    currentRound = 1;
+    roundDisplay.textContent = "Round: " + currentRound + " of " + totalRounds;
 
-    if (humanChoice === "rock") {
-        return "Rock";
-    }
-    else if(humanChoice === "paper") {
-        return "Paper";
-    }
-    else {
-        return "Scissors";
-    }
+    userScoreDisplay.textContent = "User Score: ";
+    computerScoreDisplay.textContent = "Computer Score: ";
+
+    userChoiceDisplay.textContent = "User Choice: ";
+    computerChoiceDisplay.textContent = "Computer Choice: ";
+
+    resultChoiceDisplay.textContent = "";
+    finalWinnerDisplay.textContent = "";
 }
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "paper" && computerChoice === "rock") {
-        return "You win. Paper beats Rock";
-        humanScore += 1;
-    }
-    else if(humanChoice ==="scissors" && computerChoice === "paper") {
-        return "You win. Scissors beats Paper";
-        humanScore += 1;
-    }
-    else if(humanChoice === "rock" && computerChoice === "scissors") {
-        return "You win. Rock beats Scissors";
-        humanScore += 1;
-    }
-    else if (humanChoice === computerChoice) {
-        return "You Tie. Play again";
-        humanChoice = 0;
-        computerChoice = 0;
-    }
-    else {
-        return "You Lose. " + computerChoice + "beats " + humanChoice;
-        computerChoice += 1;
-    }
-}
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice()
-        console.log(playRound(humanChoice, computerChoice));
-    }
-
-    if (humanScore > computerScore) {
-        return "You have beaten the computer";
-    }
-    else if(computerScore > humanScore) {
-        return "The computer has beaten you."
-    }
-    else {
-        return "You tied with the computer. Try again."
-    }
-}
-
-
-//let humanChoice = getHumanChoice();
-//let computerChoice = getComputerChoice();
-
-console.log(playRound);
